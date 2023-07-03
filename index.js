eaf4f609d29dbe237a16f46426dfce70c574807d
ObianuoObi <117560960+ObianuoObi@users.noreply.github.com>
2023-07-03 09:41:42 +0100

    Group points into batches of 5 for concurrrent requests

--- index.js
+++ index.js
@@ -7,4 +7,15 @@ const points = Array.from({ length: 100 }, () => ({
     lat: Math.random() * 180 - 90,
     lng: Math.random() * 360 - 180,
 }));
+
+
+// Group the points into batches of 5 for concurrent requests
+
+const batches = points.reduce((acc, cur, i) => { // acc = accumulator, cur = current value, i = index
+    if (i % 5 === 0) acc.push([]);
+    acc[acc.length - 1].push(cur);
+    return acc;
+  }, []); 
+
+
  
